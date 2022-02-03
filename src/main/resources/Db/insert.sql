7f37adc03d7a2e0d4392d161c39ec5d06ac9af63
Johnycrown <oladejijohn60@gmail.com>
2022-02-03 15:02:31 +0100

    data added to database

--- src/main/resources/Db/insert.sql
+++ src/main/resources/Db/insert.sql
@@ -1,1 +1,8 @@
 truncate table product;
+
+insert into product(id, name, price, quantity)
+values (12, 'Luxury Mop', 2430, 3),
+(13, 'Macbook Air', 18320, 4),
+(14, 'laptop', 183220, 6),
+(15, 'Mercede benz', 18353320, 4),
+(16, 'Highlander', 1878320, 8);
