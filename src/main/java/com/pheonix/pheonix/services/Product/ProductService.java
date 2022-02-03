cdff1a5cb2f1c636b759f6c1670c8aca56e7e88c
Johnycrown <oladejijohn60@gmail.com>
2022-02-03 15:53:38 +0100

    product service class implemented

--- src/main/java/com/pheonix/pheonix/services/Product/ProductService.java
+++ src/main/java/com/pheonix/pheonix/services/Product/ProductService.java
@@ -1,11 +1,14 @@
 package com.pheonix.pheonix.services.Product;
 
+import com.pheonix.pheonix.data.dto.ProductDto;
 import com.pheonix.pheonix.data.model.Product;
+import com.pheonix.pheonix.web.exceptions.ProductDoesNotExistException;
 
 import java.util.List;
 
 public interface ProductService {
     List<Product> getAllProduct();
-    Product findProductByID();
-    Product createProduct();
+    Product findProductByID(Long productId) throws ProductDoesNotExistException;
+    Product createProduct(ProductDto productDto);
+
 }
