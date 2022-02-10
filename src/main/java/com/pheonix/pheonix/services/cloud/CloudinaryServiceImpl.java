0ef5f0884365130f511998b1cf5bab6f9a57d76c
Johnycrown <oladejijohn60@gmail.com>
2022-02-10 11:17:20 +0100

    cloudinary implementation service interface was created

---
+++ src/main/java/com/pheonix/pheonix/services/cloud/CloudinaryServiceImpl.java
@@ -0,0 +1,13 @@
+package com.pheonix.pheonix.services.cloud;
+
+import java.io.File;
+import java.util.Map;
+
+public class CloudinaryServiceImpl implements CloudinaryService {
+
+    @Override
+    public Map<?, ?> upload(File file, Map<?, ?> params) {
+        return null;
+
+    }
+}
