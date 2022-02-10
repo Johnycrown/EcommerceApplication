b3959d35864122b7db1ce588b3429ee5ca524053
Johnycrown <oladejijohn60@gmail.com>
2022-02-10 11:18:28 +0100

    ProductRepository modified

--- src/main/java/com/pheonix/pheonix/data/repository/ProductRepository.java
+++ src/main/java/com/pheonix/pheonix/data/repository/ProductRepository.java
@@ -4,6 +4,9 @@ import com.pheonix.pheonix.data.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Optional<Product> findByName(String name);
 }
