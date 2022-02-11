bcbe01735fe8c6d285b4ae7cda1a7e0a233d52de
Johnycrown <oladejijohn60@gmail.com>
2022-02-11 10:43:44 +0100

    cloudinary service interface created

--- src/main/java/com/pheonix/pheonix/services/cloud/CloudinaryService.java
+++ src/main/java/com/pheonix/pheonix/services/cloud/CloudinaryService.java
@@ -1,8 +1,9 @@
 package com.pheonix.pheonix.services.cloud;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public interface CloudinaryService {
-    Map<?,?> upload(File file, Map<?,?> paramd);
+    Map<?,?> upload(File file, Map<?,?> paramd) throws IOException;
 }
