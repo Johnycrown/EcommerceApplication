7a3c74649c45089cdae902352b310bfc970078f8
Johnycrown <oladejijohn60@gmail.com>
2022-02-11 10:44:30 +0100

    cloudinary service implementation created

--- src/main/java/com/pheonix/pheonix/services/cloud/CloudinaryServiceImpl.java
+++ src/main/java/com/pheonix/pheonix/services/cloud/CloudinaryServiceImpl.java
@@ -1,13 +1,22 @@
 package com.pheonix.pheonix.services.cloud;
 
-import java.io.File;
-import java.util.Map;
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+@Service
 public class CloudinaryServiceImpl implements CloudinaryService {
+    @Autowired
+    Cloudinary cloudinary;
 
     @Override
-    public Map<?, ?> upload(File file, Map<?, ?> params) {
-        return null;
+    public Map<?, ?> upload(File file, Map<?, ?> params) throws IOException {
+
+        return cloudinary.uploader().upload(file,params);
+
 
     }
 }
