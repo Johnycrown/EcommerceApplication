d06bca0e4e1f8b4e15ced6ba6ea0b402984a6b66
Johnycrown <oladejijohn60@gmail.com>
2022-02-11 16:34:21 +0100

    configuration class created

--- src/main/java/com/pheonix/pheonix/services/cloud/CloudinaryService.java
+++ src/main/java/com/pheonix/pheonix/services/cloud/CloudinaryService.java
@@ -1,9 +1,12 @@
 package com.pheonix.pheonix.services.cloud;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 public interface CloudinaryService {
-    Map<?,?> upload(File file, Map<?,?> paramd) throws IOException;
+   // Map<?,?> upload(File file, Map<?,?> paramd) throws IOException;
+    public Map<?, ?> upload(byte[] bytes, Map<?, ?> params) throws IOException;
 }
