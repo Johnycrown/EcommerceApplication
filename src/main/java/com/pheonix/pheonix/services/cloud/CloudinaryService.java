337e5b865d0527ca08740464d6981306a6cf4634
Johnycrown <oladejijohn60@gmail.com>
2022-03-17 17:42:45 +0100

    modified

--- src/main/java/com/pheonix/pheonix/services/cloud/CloudinaryService.java
+++ src/main/java/com/pheonix/pheonix/services/cloud/CloudinaryService.java
@@ -8,5 +8,5 @@ import java.util.Map;
 
 public interface CloudinaryService {
    // Map<?,?> upload(File file, Map<?,?> paramd) throws IOException;
-    public Map<?, ?> upload(byte[] bytes, Map<?, ?> params) throws IOException;
+     Map<?, ?> upload(byte[] bytes, Map<?, ?> params) throws IOException;
 }
