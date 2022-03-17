47e7d6b61da5510ddb724fa7a86dcc5397102e86
Johnycrown <oladejijohn60@gmail.com>
2022-03-17 17:48:00 +0100

    modified

---
+++ src/main/java/com/pheonix/pheonix/security/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.pheonix.pheonix.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    RDBUserDetailsServiceImpl rdbUserDetailsService;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                    .authorizeHttpRequests()
+                     .antMatchers(HttpMethod.POST, "/api/appuser")
+                     .permitAll()
+                     .anyRequest()
+                      .authenticated()
+                       .and()
+                        .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                        .addFilter(new JWTAthorizationFilter(authenticationManager()))
+                        .exceptionHandling()
+                       .and()
+                       .sessionManagement()
+                       .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(rdbUserDetailsService)
+                                .passwordEncoder(passwordEncoder());
+    }
+
+
+     @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+}
+}
+
+
