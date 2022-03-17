3e8eaec90eb908159ca395c862b789aa58446795
Johnycrown <oladejijohn60@gmail.com>
2022-03-17 17:46:46 +0100

    modified

--- src/main/java/com/pheonix/pheonix/services/Product/ProductService.java
+++ src/main/java/com/pheonix/pheonix/services/Product/ProductService.java
@@ -1,7 +1,11 @@
 package com.pheonix.pheonix.services.Product;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import com.pheonix.pheonix.data.dto.ProductDto;
 import com.pheonix.pheonix.data.model.Product;
+import com.pheonix.pheonix.web.exceptions.BusinessLogicException;
 import com.pheonix.pheonix.web.exceptions.ProductDoesNotExistException;
 
 import java.util.List;
@@ -9,6 +13,8 @@ import java.util.List;
 public interface ProductService {
     List<Product> getAllProduct();
     Product findProductByID(Long productId) throws ProductDoesNotExistException;
-    Product createProduct(ProductDto productDto);
+    Product createProduct(ProductDto productDto) throws BusinessLogicException;
+  //  Product  updateProduct(String productName, ProductDto productDto) throws ProductDoesNotExistException;
+  Product  updateProduct(Long productName, JsonPatch patch) throws BusinessLogicException, JsonProcessingException, JsonPatchException;
 
 }
